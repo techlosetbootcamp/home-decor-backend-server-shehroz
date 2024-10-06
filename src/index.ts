dde434ba06caf7ce4f54b667a68b4fa5994955f9
Shehroz Arshad <shehrozsheri36347@gmail.com>
2024-10-06 22:35:21 +0500

    add landing page

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,36 @@ import "reflect-metadata";
 import { createYoga } from "graphql-yoga";
 import { schema } from "./graphql/schema/schema";
 import prisma from "./config/prisma";
+import express from "express";
 import { createServer } from "http";
+import path from "path";
+import fs from "fs";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "./views/index.html");
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.status(500).send("Error loading the page.");
+      return;
+    }
+    res.setHeader("Content-Type", "text/html");
+    res.send(data);
+  });
+});
 
 const yoga = createYoga({
   schema,
   context: () => ({
     prisma,
   }),
+  landingPage: false,
 });
 
-const server = createServer(yoga);
+app.use("/graphql", yoga);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
+const server = createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server ready at http://localhost:${PORT}`);
-});
+server.listen(PORT);
